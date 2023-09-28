bde250ee030ba6f46dfb1c413f18f83be3aafa92
giovabos11 <giovabos11@gmail.com>
2023-09-28 16:09:29 -0500

    Updated makefile and compilecode folder

--- Code/compilejob.cpp
+++ Code/compilejob.cpp
@@ -6,7 +6,7 @@
 void CompileJob::Execute()
 {
     std::array<char, 128> buffer;
-    std::string command = "make automated";
+    std::string command = "make project_1";
 
     // Redirect cerr to cout
     command.append(" 2>&1");
